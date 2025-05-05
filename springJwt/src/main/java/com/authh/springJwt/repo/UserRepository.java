1e4b40b861a671f90792dc0bc4c35271edba5c13
kavir <kavirmansingh7@gmail.com>
2025-05-05 19:06:57 +0545

    minor bug fixed

--- springJwt/src/main/java/com/authh/springJwt/repo/UserRepository.java
+++ springJwt/src/main/java/com/authh/springJwt/repo/UserRepository.java
@@ -2,8 +2,10 @@ package com.authh.springJwt.repo;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.authh.springJwt.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByNumber(String number);
     Optional<User> findById(Long id);
