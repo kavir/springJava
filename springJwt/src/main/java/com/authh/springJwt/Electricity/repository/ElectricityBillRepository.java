5647c989834b1396ea0c716e1bd9e61960a7ac72
kavir <kavirmansingh7@gmail.com>
2025-05-05 16:54:54 +0545

    minor bug fixed

--- springJwt/src/main/java/com/authh/springJwt/Electricity/repository/ElectricityBillRepository.java
+++ springJwt/src/main/java/com/authh/springJwt/Electricity/repository/ElectricityBillRepository.java
@@ -2,8 +2,11 @@ package com.authh.springJwt.Electricity.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.authh.springJwt.Electricity.model.ElectricityBill;
 
+@Repository
 public interface ElectricityBillRepository extends JpaRepository<ElectricityBill, Long> {
     List<ElectricityBill> findByUserId(Long userId);
     ElectricityBill findByBill(Long billId);
