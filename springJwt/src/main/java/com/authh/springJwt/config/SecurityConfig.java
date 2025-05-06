6713bebe721159da577dbbb8833bd173b12b4705
kavir <kavirmansingh7@gmail.com>
2025-05-06 17:46:03 +0545

    okay

--- springJwt/src/main/java/com/authh/springJwt/config/SecurityConfig.java
+++ springJwt/src/main/java/com/authh/springJwt/config/SecurityConfig.java
@@ -32,9 +32,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         System.out.println("siuuu___SecurityFilterChain initialized!");
-        return http
-        // .cors().and()
-        .csrf(AbstractHttpConfigurer::disable)
+        return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests(req ->
                         req.requestMatchers("/login/**", "/register/**").permitAll()
                                 .requestMatchers("/api/employees/**").hasAnyRole("USER", "ADMIN")
@@ -85,3 +83,66 @@ public class SecurityConfig implements WebMvcConfigurer {
 
 
 
+
+// package com.authh.springJwt.config;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.authentication.AuthenticationManager;
+// import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+// import org.springframework.security.config.http.SessionCreationPolicy;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import com.authh.springJwt.filter.JwtAuthenticateFilter;
+// import com.authh.springJwt.service.UserDetailServiceImp;
+
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig {
+// @Autowired
+//     private UserDetailServiceImp userDetailServiceImp;
+//     @Autowired
+//     private JwtAuthenticateFilter jwtAuthenticationFilter;
+
+//     @SuppressWarnings({ "deprecation", "removal" })
+//     @Bean
+//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//         System.out.println("siuuu___SecurityFilterChain initialized!");
+//         return http.csrf(AbstractHttpConfigurer::disable).authorizeRequests(req ->
+//                 req.requestMatchers("/login/**","/register/**").permitAll()
+//                 .requestMatchers("/api/employees/**").hasAnyRole("USER","ADMIN")
+//                 .requestMatchers("/api/wallet/**").hasAnyRole("USER","ADMIN")
+//                 .requestMatchers("/api/transactions/**").hasAnyRole("USER","ADMIN")
+//                 .requestMatchers("/api/qr/generate**").hasAnyRole("USER","ADMIN")
+//                 .requestMatchers("/ws/transactions**").hasAnyRole("USER","ADMIN")
+//                 .anyRequest()
+//                 .authenticated())
+//                 .userDetailsService(userDetailServiceImp)
+//                 .sessionManagement(session->session
+//                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+//                 .build();
+//     }
+
+//     @Bean
+//     public PasswordEncoder passwordEncoder(){
+//         return new BCryptPasswordEncoder();
+//     }
+//     @Bean
+//     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//         return authenticationConfiguration.getAuthenticationManager();
+//     }
+    
+
+//     // @Bean
+//     // public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+//     //     return configuration.getAuthenticationManager();
+//     // }
+    
+// }
