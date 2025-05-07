b2959ab5a4c0141dbf05e66490b962369dbbab49
kavir <kavirmansingh7@gmail.com>
2025-05-07 16:04:19 +0545

    okay

--- springJwt/src/main/java/com/authh/springJwt/config/SecurityConfig.java
+++ springJwt/src/main/java/com/authh/springJwt/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns("*")  // instead of allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 
     
